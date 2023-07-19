1dd2a4a7a4f6b081246745b4f8dbf7c600d8f702
yxtang <tangyunxi000@gmail.com>
2023-07-19 21:35:46 +0800

    update RRT_star

--- PythonRobotics/PathPlanning/rrt_star.py
+++ PythonRobotics/PathPlanning/rrt_star.py
@@ -5,27 +5,40 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import Tuple
 import copy
-from rrt import WorldMap, Node
+from rrt import WorldMap, Node, RRT
 
 
-class RRTStar:
-    def __init__(self, 
-                 start_config: jnp.ndarray,
-                 goal_config: jnp.ndarray,
-                 map: WorldMap,
-                 step_size: float = 0.1,
-                 max_iter: int = 500
-                 ) -> None:
-        self._start = Node(start_config)
-        self._goal = Node(goal_config)
+class NodeX(Node):
+    def __init__(self, state) -> None:
+        super().__init__(state)
+        self._cost = 0.0
         
-        self._resolution = 0.01
+    @property
+    def cost(self):
+        return self._cost
+
+
+class RRTStar(RRT):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         
-        self._map = map
-        self._node_list = []
         
-        self._step_size = step_size
-        self._max_iter = max_iter
-        self._goal_sample_rate = 5
-        
-        self._rng_key = random.PRNGKey(seed=4)
+if __name__ == '__main__':
+    
+    world_map = WorldMap([0., 2., 0., 2.])
+    start = jnp.array([0., 0.])
+    goal = jnp.array([0.8, 0.6])
+    
+    world_map.update_start(start)
+    world_map.update_goal(goal)
+    
+    obs1 = (0.5, 0.4, 0.25)
+    obs2 = (0.4, 0.8, 0.2)
+    obs3 = (0.8, 0.8, 0.15)
+    obs4 = (1.0, 0.4, 0.15)
+    world_map.add_obstacle(obs1)
+    world_map.add_obstacle(obs2)
+    world_map.add_obstacle(obs3)
+    world_map.add_obstacle(obs4)
+    
+    planner = RRTStar(start, goal, map=world_map)
