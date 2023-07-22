219b8f62462be37500c69e251c17d38aa2be3063
yxtang <tangyunxi000@gmail.com>
2023-07-22 23:50:32 +0800

    update RRT*

--- PythonRobotics/PathPlanning/rrt_star.py
+++ PythonRobotics/PathPlanning/rrt_star.py
@@ -5,12 +5,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import Tuple
 import copy, math
-from rrt import WorldMap, RRT
+from rrt import WorldMap, Node, RRT
 
 
 class RRTStar(RRT):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, connect_range, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self._connect_range = connect_range
+        
+    def _find_near_nodes(self, new_node: Node):
+        """find all the nodes in some range
+
+        Args:
+            new_node (Node): node
+        """
+        num_node = len(self._node_list) + 1
+        node_dists = [RRT._compute_node_distance(new_node, node) for node in self._node_list]
+        near_inds = [node_dists.index(i) for i in node_dists if i <= self._connect_range**2]
         
     def plan(self, animation=True, verbose=False):
         self._node_list.append(self._start)
@@ -27,6 +38,10 @@ class RRTStar(RRT):
             new_node = self._steer(nearest_node, rand_node, self._step_size)
             new_node_cost = nearest_node.cost + self._compute_node_distance(new_node, nearest_node)
             new_node.set_cost(new_node_cost)
+            
+            if not self._check_collision(nearest_node, new_node):
+                # collision-free
+                pass
             print(f"Iter: {i} || No. Nodes: {len(self._node_list)}")
         
         
@@ -48,8 +63,10 @@ if __name__ == '__main__':
     world_map.add_obstacle(obs3)
     world_map.add_obstacle(obs4)
     
-    planner = RRTStar(start, goal, map=world_map)
+    planner = RRTStar(2.0, start, goal, map=world_map)
     
-    path_solution = planner.plan()
+    print(planner._connect_range, planner._goal.state)
     
-    print(planner._goal.state)
+    # path_solution = planner.plan()
+    
+    
