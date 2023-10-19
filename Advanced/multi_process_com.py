0f2e9350888f8d2c98d9a8da73cba4c7349e65a9
YunxiTang <1155136594@link.cuhk.edu.hk>
2023-10-19 12:09:47 +0800

    add Queue

--- Advanced/multi_process_com.py
+++ Advanced/multi_process_com.py
@@ -14,25 +14,23 @@ def sub_proc(x, name, pipe):
             msg = _in_port.recv()
             print('subp: {} recieve {}'.format(name, msg))
         except EOFError:
+            print('pip port is closed.')
             break
 
 def sub_proc2(x, name, pipe):
     _in_port, _out_port = pipe
 
-    while True:
-        try:
-            _out_port.send(x)
-        except EOFError:
-            break
+    for i in range(100):
+         _out_port.send(x+i)
+    _out_port.close()
 
 
 if __name__ == '__main__':
     in_port, out_port = Pipe()
     subp = Process(target=sub_proc, args=(100, 'p1', (in_port, out_port)))
     subp2 = Process(target=sub_proc2, args=(10, 'p2', (in_port, out_port)))
-
-    subp.start()
     subp2.start()
+    subp.start()
 
     subp.join()
     subp2.join()
---
+++ Advanced/multi_process_com2.py
@@ -0,0 +1,59 @@
+from multiprocessing import Process, Queue
+import os
+import queue
+import time
+
+
+class publisher(Process):
+    def __init__(self, name, queue: Queue, **kargs):
+        super().__init__(**kargs)
+        self._name = name
+        self._queue = queue
+        self._count = 0
+
+
+    def run(self):
+        while True:
+            msg = self._count
+            try:
+                self._queue.put_nowait(msg)
+                print('{} pub {}'.format(self._name, msg))
+            except queue.Full:
+                pass
+            self._count += 1
+            time.sleep(0.001)
+            
+
+
+class subscriber(Process):
+    def __init__(self, name, queue: Queue, **kargs):
+        super().__init__(**kargs)
+        self._name = name
+        self._queue = queue
+        self._pre_msg = None
+
+
+    def run(self):
+        while True:
+            if self._queue.qsize() > 0:
+                msg = self._queue.get()
+                print('{} get {} with qsize: {}'.format(self._name, msg, self._queue.qsize()))
+                print('=========================')
+                self._pre_msg = msg
+            else:
+                print('empty queue, use last msg. {} get {}'.format(self._name, self._pre_msg))
+            time.sleep(0.02)
+
+
+if __name__ == '__main__':
+    state_queue = Queue(maxsize=1)
+    ctrl_queue = Queue(maxsize=100)
+    
+    pub = publisher('pub', state_queue)
+    sub = subscriber('sub', state_queue)
+
+    pub.start()
+    sub.start()
+
+    pub.join()
+    sub.join()
