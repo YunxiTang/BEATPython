da52ad486209539ea003e3109388ab96605b4a93
YunxiTang <1155136594@link.cuhk.edu.hk>
2023-11-05 17:25:41 +0800

    update IK controller with QP

--- PythonRobotics/RoboticControl/main_sim.py
+++ PythonRobotics/RoboticControl/main_sim.py
@@ -3,24 +3,23 @@ from ik_controller import IKController
 import numpy as np
 import mujoco
 
+from pydrake.math import RigidTransform, RollPitchYaw, RotationMatrix
+
+
 if __name__ == '__main__':
     ur5_xml = 'ur5/ur5.urdf'
     ur5_scene_xml = 'ur5/scene.xml'
     rbt_model = mujoco.MjModel.from_xml_path(ur5_scene_xml)
     robot = UR5Env(rbt_model)
 
-    ee_name = 'wrist_3_link'
-
-    ik_controller = IKController(ur5_xml, rbt_name='UR5', ee_name=ee_name, dt=0.002)
+    ik_controller = IKController(ur5_xml, rbt_name='UR5', ee_name='fake_pinch_link', dt=0.002)
 
     q_ref = np.array([0, -1.57, -1.57, -1.0, -1.57, 0])
-    goal_ee_pos = np.array([ 0.7071068, -0.7071068, 0, 0,    
-                            -0.5, 0.05, 0.85]).reshape(-1,1)
+
+    goal_ee_pos = np.array([ 0.6, 0.2, 1.1, np.pi, 0, np.pi/6]) # translation + roll-pitch-yaw
     
     obs = robot.reset(0)
     mujoco.mj_forward(robot.mj_model, robot.mj_data)
-    print('mujoco: ', robot.mj_data.body(ee_name).xpos.ravel())
-    print('mujoco: ', robot.mj_data.body(ee_name).xquat.ravel())
     q_hist = []
     ctrl_hist = []
 
@@ -29,13 +28,17 @@ if __name__ == '__main__':
         q = obs[0:6]
         qdot = obs[6:12]
         t = robot.sim_time
-        goal_ee_pos[4] = -0.5 + 0.0 * np.sin(4 * t)
-        goal_ee_pos[5] =  0.05 + 0.0 * np.cos(2 * t)
-        ctrl = ik_controller.GetControl(goal_ee_pos, q, qdot, t)
-        
+
+        goal_ee_pos[0] = 0.5 + 0.1 * np.cos(2 * t)
+        goal_ee_pos[1] = 0.3 + 0.1 * np.sin(2 * t)
+        goal_ee_pos[2] = 0.8 + 0.1 * np.cos(2 * t)
+        goal_ee_pos[5] = np.pi/6 + np.pi / 6 * np.cos(2 * t)
+
+        ctrl = ik_controller.GetQPControl(goal_ee_pos, q, qdot, t)
         mujoco.mj_forward(robot.mj_model, robot.mj_data)
-        print('mujoco: ', robot.mj_data.body(ee_name).xpos.ravel())
-        print('mujoco: ', robot.mj_data.body(ee_name).xquat.ravel())
+
+        print('mujoco: ', robot.mj_data.site('pinch').xpos.ravel() - goal_ee_pos[0:3])
+        print('mujoco: {}'.format(robot.mj_data.site('pinch').xmat.reshape(3,3)))
         
         q_hist.append(q)
         ctrl_hist.append(ctrl)
