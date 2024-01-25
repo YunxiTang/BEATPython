25e3cff50dae032f9457f2cf9337fe31e752885b
YunxiTang <1155136594@link.cuhk.edu.hk>
2024-01-25 21:13:52 +0800

    add robot stack

--- PythonRobotics/PathPlanning/cost_map.py
+++ PythonRobotics/PathPlanning/cost_map.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABCMeta
 import jax.numpy as jnp
 from rrt import Node
+from world_map import CityMap
 
 
 class ABCCostMapLayer(metaclass=ABCMeta):
@@ -16,6 +17,23 @@ class EuclideanCostMapLayer(ABCCostMapLayer):
         cost = jnp.linalg.norm(parent_node._state - child_node._state)
         return cost
     
+
+class CityCostMapLayer(ABCCostMapLayer):
+    def __init__(self, city_map: CityMap):
+        self._city_map = city_map
+        zs = [0.] + [block._size_z for block in city_map._obstacle] + [200.]
+        self._sorted_zs = sorted(zs)
+        self._sorted_z_idx = sorted(range(len(zs)), key=lambda k: zs[k])
+        print(zs, '\n', self._sorted_zs, '\n', self._sorted_z_idx)
+
+    
+    # def construct_height_map(self, height):
+    #     for i in self._sorted_zs:
+
+
+    def compute_edge_cost(self, parent_node: Node, child_node: Node):
+        return 0
+
 
 class CostMap:
     def __init__(self):
