179911a174fcb1f9ee1d534f6b70e735bb7d024b
YunxiTang <1155136594@link.cuhk.edu.hk>
2024-03-31 17:25:50 +0800

    update deep ebm

--- PythonRobotics/LearningTorch/generative_model/deep_energy_model.py
+++ PythonRobotics/LearningTorch/generative_model/deep_energy_model.py
@@ -30,15 +30,163 @@ from torchvision.datasets import MNIST
 from torchvision import transforms
 
 
+# Ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+print("Device:", device)
+
+
+class Swish(nn.Module):
+
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+    
+
+class CNNModel(nn.Module):
+
+    def __init__(self, hidden_features=32, out_dim=1, **kwargs):
+        super().__init__()
+    
+        c_hid1 = hidden_features//2
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+
+        # Series of convolutions and Swish activation functions
+        self.cnn_layers = nn.Sequential(
+                nn.Conv2d(1, c_hid1, kernel_size=5, stride=2, padding=4), # [16x16] - Larger padding to get 32x32 image
+                Swish(),
+                nn.Conv2d(c_hid1, c_hid2, kernel_size=3, stride=2, padding=1), #  [8x8]
+                Swish(),
+                nn.Conv2d(c_hid2, c_hid3, kernel_size=3, stride=2, padding=1), # [4x4]
+                Swish(),
+                nn.Conv2d(c_hid3, c_hid3, kernel_size=3, stride=2, padding=1), # [2x2]
+                Swish(),
+                nn.Flatten(),
+                nn.Linear(c_hid3*4, c_hid3),
+                Swish(),
+                nn.Linear(c_hid3, out_dim)
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
+    
+
+class Sampler:
+
+    def __init__(self, model, img_shape, sample_size, max_len=8192):
+        """
+        Inputs:
+            model - Neural network to use for modeling E_theta
+            img_shape - Shape of the images to model
+            sample_size - Batch size of the samples
+            max_len - Maximum number of data points to keep in the buffer
+        """
+        super().__init__()
+        self.model = model
+        self.img_shape = img_shape
+        self.sample_size = sample_size
+        self.max_len = max_len
+        self.examples = [(torch.rand( (1, )+img_shape )*2-1) for _ in range(self.sample_size)]
+
+    def sample_new_exmps(self, steps=60, step_size=10):
+        """
+        Function for getting a new batch of "fake" images.
+        Inputs:
+            steps - Number of iterations in the MCMC algorithm
+            step_size - Learning rate nu in the algorithm above
+        """
+        # Choose 95% of the batch from the buffer, 5% generate from scratch
+        n_new = np.random.binomial(self.sample_size, 0.05)
+        rand_imgs = torch.rand((n_new,) + self.img_shape) * 2 - 1
+        old_imgs = torch.cat(random.choices(self.examples, k=self.sample_size-n_new), dim=0)
+        inp_imgs = torch.cat([rand_imgs, old_imgs], dim=0).detach().to(device)
+
+        # Perform MCMC sampling
+        inp_imgs = Sampler.generate_samples(self.model, inp_imgs, steps=steps, step_size=step_size)
+
+        # Add new images to the buffer and remove old ones if needed
+        self.examples = list(inp_imgs.to(torch.device("cpu")).chunk(self.sample_size, dim=0)) + self.examples
+        self.examples = self.examples[:self.max_len]
+        return inp_imgs
+
+    @staticmethod
+    def generate_samples(model, inp_imgs, steps=60, step_size=10, return_img_per_step=False):
+        """
+        Function for sampling images for a given model.
+        Inputs:
+            model - Neural network to use for modeling E_theta
+            inp_imgs - Images to start from for sampling. If you want to generate new images, enter noise between -1 and 1.
+            steps - Number of iterations in the MCMC algorithm.
+            step_size - Learning rate nu in the algorithm above
+            return_img_per_step - If True, we return the sample at every iteration of the MCMC
+        """
+        # Before MCMC: set model parameters to "required_grad=False"
+        # because we are only interested in the gradients of the input.
+        is_training = model.training
+        model.eval()
+        for p in model.parameters():
+            p.requires_grad = False
+        inp_imgs.requires_grad = True
+
+        # Enable gradient calculation if not already the case
+        had_gradients_enabled = torch.is_grad_enabled()
+        torch.set_grad_enabled(True)
+
+        # We use a buffer tensor in which we generate noise each loop iteration.
+        # More efficient than creating a new tensor every iteration.
+        noise = torch.randn(inp_imgs.shape, device=inp_imgs.device)
+
+        # List for storing generations at each step (for later analysis)
+        imgs_per_step = []
+
+        # Loop over K (steps)
+        for _ in range(steps):
+            # Part 1: Add noise to the input.
+            noise.normal_(0, 0.005)
+            inp_imgs.data.add_(noise.data)
+            inp_imgs.data.clamp_(min=-1.0, max=1.0)
+
+            # Part 2: calculate gradients for the current input.
+            out_imgs = -model(inp_imgs)
+            out_imgs.sum().backward()
+            inp_imgs.grad.data.clamp_(-0.03, 0.03) # For stabilizing and preventing too high gradients
+
+            # Apply gradients to our current samples
+            inp_imgs.data.add_(-step_size * inp_imgs.grad.data)
+            inp_imgs.grad.detach_()
+            inp_imgs.grad.zero_()
+            inp_imgs.data.clamp_(min=-1.0, max=1.0)
+
+            if return_img_per_step:
+                imgs_per_step.append(inp_imgs.clone().detach())
+
+        # Reactivate gradients for parameters for training
+        for p in model.parameters():
+            p.requires_grad = True
+        model.train(is_training)
+
+        # Reset gradient calculation to setting before this function
+        torch.set_grad_enabled(had_gradients_enabled)
+
+        if return_img_per_step:
+            return torch.stack(imgs_per_step, dim=0)
+        else:
+            return inp_imgs
+
+
 if __name__ == '__main__':
-    # training dataset
-    N = 500
-    sub_dist_p1 = torch.distributions.Normal(loc=-5, scale=2.0)
-    sub_dist_p2 = torch.distributions.Normal(loc=5, scale=2.0)
-    sampled_sub_data1 = sub_dist_p1.sample([N, 1])
-    sampled_sub_data2 = sub_dist_p2.sample([N, 1])
-    sampled_data = torch.concat( [sampled_sub_data1, sampled_sub_data2], dim=0)
-    fig, ax = plt.subplots(1, 2)
-    ax[0].scatter( sampled_data, np.zeros( sampled_data.shape ) )
-    ax[0].hist(sampled_data.flatten(), bins=30, density=True)
-    plt.show()
+    # Transformations applied on each image => make them a tensor and normalize between -1 and 1
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,))
+                                ])
+
+    # Loading the training dataset. We need to split it into a training and validation part
+    train_set = MNIST(root='./dataset', train=True, transform=transform, download=True)
+    # Loading the test set
+    test_set = MNIST(root='./dataset', train=False, transform=transform, download=True)
+
+    train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True,  drop_last=True,  num_workers=4, pin_memory=True)
+    test_loader  = data.DataLoader(test_set,  batch_size=256, shuffle=False, drop_last=False, num_workers=4)
