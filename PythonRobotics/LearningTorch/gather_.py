202cd3b3b2b546425a3388409a316a98fdda881e
YunxiTang <1155136594@link.cuhk.edu.hk>
2024-03-31 17:43:55 +0800

    update gather_

--- PythonRobotics/LearningTorch/gather_.py
+++ PythonRobotics/LearningTorch/gather_.py
@@ -1,6 +1,18 @@
 import torch
+from einops import rearrange, repeat
+import torch.nn as nn
+
 
 if __name__ == '__main__':
+
+    cls_token = nn.Parameter(torch.rand(1, 1, 7), requires_grad=True)
+    repeated_cls = cls_token.repeat(2, 1, 1)
+    repeated_cls_v2 = repeat(cls_token, 'a b c -> (repeat a) b c', repeat=2)
+    print(cls_token)
+    print(repeated_cls - repeated_cls_v2)
+    print(repeated_cls.shape)
+    print(repeated_cls_v2.shape)
+
     tensor_0 = torch.randint(0, 5, (2, 5, 3))
     print(tensor_0)
     index = torch.tensor([[2, 1, 0],
