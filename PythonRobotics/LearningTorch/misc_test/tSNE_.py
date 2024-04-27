6c70542fd4b76ed252ae139877ff9650301d1a35
YunxiTang <1155136594@link.cuhk.edu.hk>
2024-04-27 13:48:01 +0800

    add t-SNE

---
+++ PythonRobotics/LearningTorch/misc_test/tSNE_.py
@@ -0,0 +1,17 @@
+from sklearn import datasets
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+
+iris_df = datasets.load_iris()
+
+model = TSNE(learning_rate=100)
+
+transformed = model.fit_transform(iris_df.data)
+
+x_axis = transformed[:, 0]
+y_axis = transformed[:, 1]
+
+plt.scatter(x_axis, y_axis, c=iris_df.target)
+
+plt.show()
