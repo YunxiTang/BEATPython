498c0e334aaa41f8bbd127163d8826bb02a34f12
YunxiTang <1155136594@link.cuhk.edu.hk>
2024-09-26 13:23:59 +0800

    update obj.__dict__()

--- PythonRobotics/LearningJax/rt-x/rt-1.py
+++ PythonRobotics/LearningJax/rt-x/rt-1.py
@@ -9,3 +9,13 @@ import numpy as np
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
+
+
+class VisualEncdoder(nn.Module):
+    feature_dim: int
+
+    def setup(self) -> None:
+        return super().setup()
+    
+    def __call__(self, *args, **kwargs) -> enum.Any:
+        return super().__call__(*args, **kwargs)
