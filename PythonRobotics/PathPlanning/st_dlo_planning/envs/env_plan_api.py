20bd68a97e10a91a61f2b67fd9f53f24c40cf92f
yxtang <tangyunxi000@gmail.com>
2024-11-14 20:22:52 +0800

    add env for st_dlo

---
+++ PythonRobotics/PathPlanning/st_dlo_planning/envs/env_plan_api.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class Obstacle:
+    obs_type: str = None
+    obs_pos: list = []
+    obs_orient: list = []
+    size: list = []
+    
+
+class Env:
+    '''
+        extract config for planner
+    '''
+    def __init__(self, xml_path):
+        self.xml_path = xml_path
+        
+        
+    def get_obstacles(self):
+        return []
