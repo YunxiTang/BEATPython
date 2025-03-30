65714b98d061dd765c7b500d7ce584c88ee0a363
YunxiTang <1155136594@link.cuhk.edu.hk>
2025-03-30 14:55:51 +0800

    add comparison

--- PythonRobotics/PathPlanning/st_dlo_planning/neural_mpc_tracker/configuration_gdm.py
+++ PythonRobotics/PathPlanning/st_dlo_planning/neural_mpc_tracker/configuration_gdm.py
@@ -17,3 +17,36 @@ class GDM_CFG:
     num_eef: int = 2
     eef_dim: int = 3
     delta_eef_dim: int = 3
+
+
+@dataclass
+class CONV_GDM_CFG:
+    # dlo 
+    max_kp: int = 13
+    kp_dim: int = 2
+
+    embed_dim: int = 128
+    
+    # eefs
+    num_eef: int = 2
+    eef_dim: int = 3
+    delta_eef_dim: int = 3
+
+
+@dataclass
+class RNN_GDM_CFG:
+    # dlo 
+    max_kp: int = 13
+    kp_dim: int = 2
+    conv1d_ngroup: int = 4
+    conv1d_kernel_size: int = 1
+    
+    # rnn
+    rnn_hidden_size: int = 128
+    rnn_num_layers: int = 2
+    rnn_dropout: float = 0.1
+    
+    # eefs
+    num_eef: int = 2
+    eef_dim: int = 3
+    delta_eef_dim: int = 3
